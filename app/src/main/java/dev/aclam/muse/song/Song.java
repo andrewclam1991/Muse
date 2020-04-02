1a92f98fdc4e85d09981dc60d3b1c5f355867668
Andrew lam <23492362+andrewclam1991@users.noreply.github.com>
2020-04-01 20:35:32 -0700

    adds song bean and player service setup/cleanup implementation

--- app/src/main/java/dev/aclam/muse/song/Song.java
+++ app/src/main/java/dev/aclam/muse/song/Song.java
@@ -3,5 +3,38 @@ package dev.aclam.muse.song;
 public class Song {
     private String uuid;
     private String title;
+    private String author;
     private String uri;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
 }
--- player/src/main/java/dev/aclam/player/ExoPlayerService.java
+++ player/src/main/java/dev/aclam/player/ExoPlayerService.java
@@ -2,26 +2,30 @@ package dev.aclam.player;
 
 import android.app.Service;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 
 public class ExoPlayerService extends Service implements PlayerService {
 
+    private static final String TAG = ExoPlayerService.class.getName();
+
+    private PlaybackStateListener playbackStateListener;
     private SimpleExoPlayer player;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
+    private boolean playWhenReady;
+    private int currentWindow;
+    private long playbackPosition;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -39,6 +43,18 @@ public class ExoPlayerService extends Service implements PlayerService {
     public boolean onUnbind(Intent intent) {
         // start foreground service
         return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setupPlayer();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cleanupPlayer();
     }
 
     @Override
@@ -68,17 +84,70 @@ public class ExoPlayerService extends Service implements PlayerService {
 
     @Override
     public void seekTo(int position) {
-        player.seekTo(0, position);
+        player.seekTo(currentWindow, position);
     }
 
-    /**
-     * Initializes a
-     */
-    private void initializePlayer() {
+    private void setupPlayer() {
         if (player == null) {
             DefaultTrackSelector trackSelector = new DefaultTrackSelector();
             trackSelector.setParameters(trackSelector.buildUponParameters().setMaxVideoSizeSd());
             player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+        }
+
+        playbackStateListener = new PlaybackStateListener();
+
+        // TODO get default uri from database
+        Uri uri = Uri.parse(getString(R.string.media_url_mp3));
+        MediaSource mediaSource = buildMediaSource(uri);
+
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+        player.addListener(playbackStateListener);
+        player.prepare(mediaSource, false, false);
+    }
+
+    private void cleanupPlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.removeListener(playbackStateListener);
+            player.release();
+            player = null;
+        }
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        // TODO what is a userAgent in this context?
+        DataSource.Factory dataSourceFactory =
+                new DefaultDataSourceFactory(this, "toHear");
+        DashMediaSource.Factory mediaSourceFactory = new DashMediaSource.Factory(dataSourceFactory);
+        return mediaSourceFactory.createMediaSource(uri);
+    }
+
+    private static class PlaybackStateListener implements Player.EventListener {
+        @Override
+        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+            String stateString;
+            switch (playbackState) {
+                case ExoPlayer.STATE_IDLE:
+                    stateString = "ExoPlayer.STATE_IDLE      -";
+                    break;
+                case ExoPlayer.STATE_BUFFERING:
+                    stateString = "ExoPlayer.STATE_BUFFERING -";
+                    break;
+                case ExoPlayer.STATE_READY:
+                    stateString = "ExoPlayer.STATE_READY     -";
+                    break;
+                case ExoPlayer.STATE_ENDED:
+                    stateString = "ExoPlayer.STATE_ENDED     -";
+                    break;
+                default:
+                    stateString = "UNKNOWN_STATE             -";
+                    break;
+            }
+            Log.d(TAG, "changed state to " + stateString
+                    + " playWhenReady: " + playWhenReady);
         }
     }
 
